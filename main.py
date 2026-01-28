a16ddd86d90bf8457c3bb4657fde5cd766cc0a74
Caiquef80 <caiquecampos.767@gmail.com>
2026-01-28 10:01:40 -0300

    alterações

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow , QMessageBox
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.uic import loadUi
+from PyQt5.QtGui import QPixmap
 from pyshorteners import Shortener
 from notifypy import Notify
+import qrcode 
 
 
 def CREATE_SHORT_URL(url):
   link = Shortener()
   return link.tinyurl.short(url)
+
+def CREATE_QRCODE(url):
+  img = qrcode.make(url)
+  return img.save("teste.png")
     
 class QRCodeBuilder(QMainWindow):
     def __init__(self, **kwargs):
@@ -28,14 +34,15 @@ class QRCodeBuilder(QMainWindow):
       valor = self.getURL()
       if valor:
         url = CREATE_SHORT_URL(valor)
+        CREATE_QRCODE(url)
         self.setURL(url)
+        self.img.setPixmap(QPixmap("teste.png"))
       else:
         self.showMessage("Erro URL" , "É esperado que você passe uma URL")
         
     def showMessage(self , title , message):
       QMessageBox.information(self, title , message)
       
-
 
 if __name__ == "__main__":
     app = QApplication([])
